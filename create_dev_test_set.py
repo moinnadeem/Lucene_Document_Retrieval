c8f4c629d4ebfc45c4b92c0a96025b4dc4b25ba7
Moin Nadeem <mnadeem@mit.edu>
2018-10-14 11:52:25 -0400

    adding new notebooks

--- create_dev_test_set.py
+++ create_dev_test_set.py
@@ -6,8 +6,8 @@ from multiprocessing import cpu_count
 import json
 from tqdm import tqdm_notebook
 
-print("Beginning transformation...")
-claims, labels, article_list, claim_set, claim_to_article = utils.extract_fever_jsonl_data("shared_task_dev.jsonl")
+#print("Beginning transformation...")
+#claims, labels, article_list, claim_set, claim_to_article = utils.extract_fever_jsonl_data("shared_task_test.jsonl")
 
 def retrieve_documents(claim, k):
     cleaned_claim = claim.replace("/", " ")
@@ -21,11 +21,11 @@ def retrieve_documents(claim, k):
         filenames.append("http://wikipedia.org/wiki/{}".format(a))
     return {"claim": claim, "evidence": filenames}
 
-result = Parallel(n_jobs=8, verbose=1)(delayed(retrieve_documents)(c, 400) for c in claims)
+#result = Parallel(n_jobs=8, verbose=1)(delayed(retrieve_documents)(c, 400) for c in claims)
 
-print("Dumping shared dev task...")
-with open("shared_task_dev.pkl", "wb") as f:
-    pickle.dump(result, f)
+#print("Dumping shared dev task...")
+#with open("shared_task_dev.pkl", "wb") as f:
+#    pickle.dump(result, f)
 
 print("Beginning test task...")
 claims = []
